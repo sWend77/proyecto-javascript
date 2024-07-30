d5b45636d69871df11c2f7d18478f44d00afc24f
Diego Aguilar <diego-98.agg@hotmail.com>
2024-07-29 23:56:43 -0300

    Mejorando algoritmo con posibles alternativas

--- js/funciones.js
+++ js/funciones.js
@@ -4,29 +4,50 @@ console.log("Calcule el precio total del producto que desea comprar con impuesto
 
 let precio = (prompt("Ingrese el precio del producto o salga del programa escribiendo 'exit': "));
 
-console.log("El precio del producto es $" + precio);
-
 while (precio != "exit") {
 
-    precio = parseFloat(precio);
+    if (precio === "") {
+        
+        alert("No ha ingresado ningún valor. Ingrese algún número en el siguiente intento.");
+        
+        console.warn("No ha ingresado ningún valor. Ingrese algún número en el siguiente intento.");
+
+        break;
     
-    const impuestoPais = x => x * 0.30;
+    } else if (isNaN(precio)){
+        
+        alert("No ha ingresado un valor numérico. Inténtelo nuevamente.");
+        
+        console.warn("No ha ingresado un valor numérico. Inténtelo nuevamente.");
 
-    const iva = x => x * 0.21; 
-
-    console.log ("El impuesto pais es de $"+ impuestoPais(precio))
-
-    console.log("El iva es de $" + iva(precio)) 
+        break;
     
-    let precioTotal = x => precio + iva(precio) + impuestoPais(precio)
+    } else{ 
 
-    console.log("El precio total con iva e impuesto pais incluido es de $" + precioTotal())
+        console.log("El precio del producto es $" + precio);
 
-    precio = prompt("Ingrese un nuevo precio si desea : ");
+        precio = parseFloat(precio);
+                    
+        const impuestoPais = x => x * 0.30;
 
+        const iva = x => x * 0.21; 
+
+        console.log ("El impuesto pais es de $"+ impuestoPais(precio))
+
+        console.log("El iva es de $" + iva(precio)) 
+                    
+        let precioTotal = x => precio + iva(precio) + impuestoPais(precio)
+
+        console.log("El precio total con iva e impuesto pais incluidos es de $" + precioTotal())
+
+        precio = prompt("Ingrese un nuevo precio si desea continuar o escriba 'exit': ");
+    }
 }
 
+
 if (precio == "exit"){
+        
     alert("Programa finalizado");
+        
     console.log("Programa finalizado")
 }
