446e24635b243a6cf13cf6aa5bf804afa1f83caf
Diego Aguilar <diego-98.agg@hotmail.com>
2024-08-28 21:56:40 -0300

    Agregando diseño a estructura html

--- js/a-modelo-registro.js
+++ js/a-modelo-registro.js
@@ -1,24 +1,34 @@
 let listaUsuarios = [];
 
 alert("Bienvenido a Gamedstore!")
+
 alert("Registrese a continuacion...")
 
 console.log("Bienvenido a Gamedstore!")
+
 console.log("Registrese a continuacion...")
 
 class Usuario {
 
     constructor(nombreCompleto, nombreUsuario, email, contrasenia, edad, direccion){
+        
         this.nombreCompleto = nombreCompleto;
+        
         this.nombreUsuario = nombreUsuario;
+        
         this.email = email;
+        
         this.contrasenia = contrasenia;
+        
         this.edad = edad;
+        
         this.direccion = direccion;
     };
     chequeoRegistro(){
         if (listaUsuarios.some(usuario => usuario.email === email)) {
+            
             alert("El usuario con este correo electrónico ya está registrado.");
+            
             return;
         }
     };
@@ -27,23 +37,33 @@ class Usuario {
 let registrarse = () => {
 
     let nombreCompleto = prompt("Nombre completo: ");
+    
     let nombreUsuario = prompt("Nombre de usuario: ");
+    
     let email = prompt("Email: ");
+    
     let contrasenia = prompt("Contraseña: ");
 
     if (!nombreCompleto || !nombreUsuario || !email || !contrasenia) {
+        
         alert("Todos los campos son obligatorios.");
+        
         return;
     }
 
     const emailRegex = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
+    
     if (!emailRegex.test(email)) {
+        
         alert("Por favor, ingrese un email válido.");
+        
         return;
     }
 
     if (contrasenia.length < 6) {
+        
         alert("La contraseña debe tener al menos 6 caracteres.");
+        
         return;
     }
 
@@ -71,18 +91,25 @@ let iniciarSesion = () =>{
     let contraseña = prompt("Contraseña: ");
 
     if (!ingresarEmail || !contraseña) {
+        
         alert("Todos los campos son obligatorios.");
+        
         return;
     };
 
     let usuario = listaUsuarios.find(usuario => usuario.email === ingresarEmail);
 
     if (usuario && usuario.contrasenia === contraseña) {
-        alert("Inicio de sesión exitoso. ¡Bienvenido de nuevo, " + usuario.nombreUsuario + "!");
-        console.log("Inicio de sesión exitoso. ¡Bienvenido de nuevo, " + usuario.nombreUsuario + "!");
+        
+        alert("¡Bienvenido de nuevo, " + usuario.nombreUsuario + "! Sesion iniciada exitosamente.");
+        
+        console.log("¡Bienvenido de nuevo, " + usuario.nombreUsuario + "! Sesion iniciada exitosamente.");
     } else {
+        
         alert("Email o contraseña incorrectos. Por favor, inténtelo nuevamente.");
+        
         console.log("Email o contraseña incorrectos. Por favor, inténtelo nuevamente.");
+        
         iniciarSesion();
 
     };
